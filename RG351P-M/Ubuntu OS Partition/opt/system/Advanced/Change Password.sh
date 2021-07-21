4106164581985f685d20c40e529c91b1e7241a92
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-21 16:15:29 -0500

    Update Change Password.sh

--- RG351P-M/Ubuntu OS Partition/opt/system/Advanced/Change Password.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Advanced/Change Password.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 sudo rg351p-js2xbox --silent -t oga_joypad &
 sleep 1
 sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 sleep 1
 sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 
-OLD_PASS=$`osk "Enter OLD password." | tail -n 1`
-NEW_PASS=$`osk "Enter NEW password." | tail -n 1`
+OLD_PASS=$( osk "Enter OLD password." | tail -n 1 )
+NEW_PASS=$( osk "Enter NEW password." | tail -n 1 )
 
-echo "$OLD_PASS : $NEW_PASS"
-echo -e "$OLD_PASS\n$NEW_PASS\n$NEW_PASS" | (passwd ark)
+echo "${OLD_PASS} : ${NEW_PASS}"
+echo -e "${OLD_PASS}\n${NEW_PASS}\n${NEW_PASS}" | (passwd ark)
 
 status=$?
 
@@ -20,5 +21,5 @@ else
 	msgbox "Password change failed."
 fi
 
-sudo kill $(pidof rg351p-js2xbox)
+sudo kill "$(pidof rg351p-js2xbox)"
 sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
