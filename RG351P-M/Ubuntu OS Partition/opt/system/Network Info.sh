c0cf993fa3cc36fdc75f737b027f93ebf4cb51b0
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-21 18:04:09 -0500

    Functionalization and some changes

--- RG351P-M/Ubuntu OS Partition/opt/system/Network Info.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Network Info.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-sudo rg351p-js2xbox --silent -t oga_joypad &
-sleep 1
-sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-sleep 1
-sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+# Start rg351-p-js2xbox for i/o
+start_oga () {
+	sudo rg351p-js2xbox --silent -t oga_joypad &
+	sleep 1
+	sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+	sleep 1
+	sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+}
 
-arr=($(nmcli -g uuid c s --active))
+# Cleanup
+cleanup () {
+	sudo kill "$(pidof rg351p-js2xbox)"
+	sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+}
 
-for i in "${arr[@]}"
-do
-	#echo uuid=$i
-	str=$(echo CONNECTION)
-	str+=$'\n'
-	str+=$(nmcli -g connection.id c s $i)
-	str+=$'\n\n'
-	str+=$(nmcli -m tabular -f ip4.address,ip4.gateway,ip4.domain c s $i)
+# Main
+main () {
+	start_oga
 
-	echo $str
-	msgbox "$str"
-done
-sudo kill $(pidof rg351p-js2xbox)
-sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+	array=("$(nmcli -g uuid c s --active)")
+
+	for i in "${array[@]}"; do
+		connection_id=$( nmcli -g connection.id c s "${i}" )
+		connection_info=$( nmcli -m tabular -f ip4.address,ip4.gateway,ip4.domain c s "${i}" )
+		msgbox "$( printf "CONNECTION\n%s\n\n%s" "${connection_id}" "${connection_info}" )"
+	done
+
+	cleanup
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
