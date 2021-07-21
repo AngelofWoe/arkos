6a0ceef4850674d4e3b244d968d5b06f4146b83e
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-21 16:18:14 -0500

    Use recommended exit code check

--- RG351P-M/Ubuntu OS Partition/opt/system/Advanced/Backup Settings.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Advanced/Backup Settings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 clear
 
 LOG_FILE="/roms/backup/arkosbackup.log"
@@ -19,8 +20,7 @@ touch $LOG_FILE
 sudo chmod 666 /dev/tty1
 tail -f $LOG_FILE >> /dev/tty1 &
 
-sudo tar -zchvf /roms/backup/arkosbackup.tar.gz /etc/localtime /etc/NetworkManager/system-connections /home/ark/.config/retroarch/retroarch.cfg /home/ark/.config/retroarch/config /home/ark/.config/retroarch32/retroarch.cfg /home/ark/.config/retroarch32/config /home/ark/.emulationstation/collections /home/ark/.emulationstation/es_settings.cfg /opt/amiberry/savestates /opt/amiberry/whdboot /opt/mupen64plus/InputAutoCfg.ini /opt/drastic/config/drastic.cfg | tee -a "$LOG_FILE"
-if [ $? -eq 0 ]; then
+if ! sudo tar -zchvf /roms/backup/arkosbackup.tar.gz /etc/localtime /etc/NetworkManager/system-connections /home/ark/.config/retroarch/retroarch.cfg /home/ark/.config/retroarch/config /home/ark/.config/retroarch32/retroarch.cfg /home/ark/.config/retroarch32/config /home/ark/.emulationstation/collections /home/ark/.emulationstation/es_settings.cfg /opt/amiberry/savestates /opt/amiberry/whdboot /opt/mupen64plus/InputAutoCfg.ini /opt/drastic/config/drastic.cfg | tee -a "$LOG_FILE"; then
 	printf "\n\n\e[32mThe backup completed successfuly. \nYour settings backup is located in the backup folder on the easyroms partition and is named arkosbackup.tar.gz. \nKeep it somewhere safe! \n"  | tee -a "$LOG_FILE"
 	printf "\033[0m"  | tee -a "$LOG_FILE"
 	sleep 10
