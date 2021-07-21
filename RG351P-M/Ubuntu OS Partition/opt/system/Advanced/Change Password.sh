c91fa10500f5181637336096a6d17bcafa581354
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-21 17:08:31 -0500

    Functionalizing

--- RG351P-M/Ubuntu OS Partition/opt/system/Advanced/Change Password.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Advanced/Change Password.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-sudo rg351p-js2xbox --silent -t oga_joypad &
-sleep 1
-sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-sleep 1
-sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+# Start rg351p-js2xbox for input
+start_oga () {
+	sudo rg351p-js2xbox --silent -t oga_joypad &
+	sleep 1
+	sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+	sleep 1
+	sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+}
 
-OLD_PASS=$( osk "Enter OLD password." | tail -n 1 )
-NEW_PASS=$( osk "Enter NEW password." | tail -n 1 )
+# Ask for old password and new password then attempt to change it
+change_pass () {
+	OLD_PASS=$( osk "Enter OLD password." | tail -n 1 )
+	NEW_PASS=$( osk "Enter NEW password." | tail -n 1 )
 
-echo "${OLD_PASS} : ${NEW_PASS}"
-echo -e "${OLD_PASS}\n${NEW_PASS}\n${NEW_PASS}" | (passwd ark)
+	echo "${OLD_PASS} : ${NEW_PASS}"
 
-status=$?
+	if echo -e "${OLD_PASS}\n${NEW_PASS}\n${NEW_PASS}" | (passwd ark); then
+		msgbox "Password successfully changed."
+	else
+		msgbox "Password change failed."
+	fi
+}
 
-if test $status -eq 0
-then
-	msgbox "Password successfully changed."
-else
-	msgbox "Password change failed."
+# Kill rg351p-js2xbox and rm input file
+cleanup () {
+	sudo kill "$(pidof rg351p-js2xbox)"
+	sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+}
+
+# Main
+main () {
+	start_oga
+	change_pass
+	cleanup
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
 fi
-
-sudo kill "$(pidof rg351p-js2xbox)"
-sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
