ed2b642e0f4a809747dcbda4558dac77c7485f4d
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-21 18:57:45 -0500

    More cleanup

--- RG351P-M/Ubuntu OS Partition/opt/system/USB Drive Unmount.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/USB Drive Unmount.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-sudo umount /mnt/usbdrive
-status=$?
-if test $status -eq 0
-then
-	printf "\n\n\e[32mUSB drive has been safely unmounted from /mnt/usbdrive...\n"
-	printf "\033[0m"
-	sleep 3
-else
-	printf "\n\n\e[91mThere was no USB drive available to unmount from /mnt/usbdrive...\n"
-	printf "\033[0m"
-	sleep 3
+
+# Main
+main () {
+	sudo umount /mnt/usbdrive
+	status=$?
+	if test $status -eq 0
+	then
+		printf "\n\n\e[32mUSB drive has been safely unmounted from /mnt/usbdrive...\n"
+		printf "\033[0m"
+		sleep 3
+	else
+		printf "\n\n\e[91mThere was no USB drive available to unmount from /mnt/usbdrive...\n"
+		printf "\033[0m"
+		sleep 3
+	fi
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
 fi
--- RG351P-M/Ubuntu OS Partition/opt/system/Wifi.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Wifi.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-sudo rg351p-js2xbox --silent -t oga_joypad &
-sleep 1
-sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-sleep 1
-sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-sudo nmui
-sudo kill $(pidof rg351p-js2xbox)
-sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+
+# Main
+main () {
+    sudo rg351p-js2xbox --silent -t oga_joypad &
+    sleep 1
+    sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+    sleep 1
+    sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+    sudo nmui
+    sudo kill "$(pidof rg351p-js2xbox)"
+    sudo rm /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
