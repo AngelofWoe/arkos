684f8b3713de0fdf9f7d3cf8c4b2300c33492e6c
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-21 18:52:20 -0500

    Make launchimage a toggle to a config instead of switching a bunch of files around

---
+++ RG351P-M/Ubuntu OS Partition/opt/system/Toggle Launchimage between ASCII and JPG.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Main
+main () {
+    # Source config file
+    if . /home/ark/.config/launchimage.cfg; then
+        if [[ "${launchimage}" == "jpg" ]]; then # Switch from JPG to ASCII
+            printf "\n\e[32mChanging to launchimage.ascii\n"
+            sleep 2
+            echo 'launchimage="ascii"' | tee /home/ark/.config/launchimage.cfg > /dev/null
+            printf "\033c" | tee -a /dev/tty1
+            sudo systemctl restart emulationstation
+        elif [[ "${launchimage}" == "ascii" ]]; then # Switch from ASCII to JPG
+            printf "\n\e[32mChanging to launchimage.jpg\n"
+            sleep 2
+            echo 'launchimage="jpg"' | tee /home/ark/.config/launchimage.cfg > /dev/null
+            printf "\033c" | tee -a /dev/tty1
+            sudo systemctl restart emulationstation
+        fi
+    else
+        printf "n\e[32mUnable to locate configuration file\n"
+    fi
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
--- RG351P-M/Ubuntu OS Partition/usr/local/bin/Switch Launchimage to ascii.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo cp -v /usr/local/bin/perfnorm.asc /usr/local/bin/perfnorm
-sudo cp -v /usr/local/bin/perfmax.asc /usr/local/bin/perfmax
-sudo chmod 777 /usr/local/bin/perfnorm
-sudo chmod 777 /usr/local/bin/perfmax
-sudo cp /usr/local/bin/Switch\ Launchimage\ to\ jpg.sh /opt/system/.
-sudo rm /opt/system/Switch\ Launchimage\ to\ ascii.sh
-printf "\033c" >> /dev/tty1
-sudo systemctl restart emulationstation
