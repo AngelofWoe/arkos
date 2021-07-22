bb631968b77abd53bfe2b4c4bf791c8efed96a4a
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-22 01:44:14 -0500

    OMG #!/bin/bash is your friend please don't forget this in bash scripts please

--- RG351P-M/EASYROMS Partition/ports/Quake 2.sh
+++ RG351P-M/EASYROMS Partition/ports/Quake 2.sh
@@ -1,1 +1,11 @@
-/usr/local/bin/retroarch32 -L /home/ark/.config/retroarch32/cores/vitaquake2_libretro.so /roms/ports/quake2/baseq2/
+#!/bin/bash
+
+# Main
+main () {
+    /usr/local/bin/retroarch32 -L /home/ark/.config/retroarch32/cores/vitaquake2_libretro.so /roms/ports/quake2/baseq2/
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
--- RG351P-M/EASYROMS Partition/ports/Quake.sh
+++ RG351P-M/EASYROMS Partition/ports/Quake.sh
@@ -1,1 +1,11 @@
-/usr/local/bin/retroarch32 -L /home/ark/.config/retroarch32/cores/tyrquake_libretro.so /roms/ports/quake/quakepaks/
+#!/bin/bash
+
+# Main
+main () {
+    /usr/local/bin/retroarch32 -L /home/ark/.config/retroarch32/cores/tyrquake_libretro.so /roms/ports/quake/quakepaks/
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
