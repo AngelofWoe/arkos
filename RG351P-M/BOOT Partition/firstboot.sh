996fc5c2075d069e7a46b8a1acae9b190b5ab804
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-22 03:57:49 -0500

    Minor changes

--- RG351P-M/BOOT Partition/firstboot.sh
+++ RG351P-M/BOOT Partition/firstboot.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-sudo rg351p-js2xbox --silent -t oga_joypad &
-sleep 1
-sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-sleep 1
-sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
-sleep 1
-sudo chmod 666 /dev/tty1
-sleep 1
-/boot/expandtoexfat.sh > /dev/tty1 2>&1
+
+# Main
+main () {
+    sudo rg351p-js2xbox --silent -t oga_joypad &
+    sleep 1
+    sudo ln -s /dev/input/event3 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+    sleep 1
+    sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
+    sleep 1
+    sudo chmod 666 /dev/tty1
+    sleep 1
+    /boot/expandtoexfat.sh > /dev/tty1 2>&1
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
