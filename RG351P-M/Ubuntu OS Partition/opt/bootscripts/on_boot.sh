6a800ef9e9df3737eba2c06ce18fb825326af535
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-22 03:16:22 -0500

    Aggravating power LED issues,.

--- RG351P-M/Ubuntu OS Partition/opt/bootscripts/on_boot.sh
+++ RG351P-M/Ubuntu OS Partition/opt/bootscripts/on_boot.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo -n 0 > /sys/devices/platform/gpio_leds/leds/blue\:heartbeat/brightness
+# Main
+main () {
+    sudo chmod 777 /sys/class/gpio/export
+    echo 77 | sudo tee /sys/class/gpio/export > /dev/null
+    sudo chmod 777 /sys/class/gpio/gpio77/direction
+    echo out | sudo tee /sys/class/gpio/gpio77/direction > /dev/null
+    sudo chmod 777 /sys/class/gpio/gpio77/value
+    echo 0 | sudo tee /sys/class/gpio/gpio77/value > /dev/null
+}
+
+# Make sure script is running directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
