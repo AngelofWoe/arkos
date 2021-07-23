1729485291d64483e0eb7404491c420a429c78d1
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-23 13:58:33 -0500

    Meh on using the config file for this

--- RG351P-M/Ubuntu OS Partition/opt/system/Connectivity/Toggle Wifi.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Connectivity/Toggle Wifi.sh
@@ -2,19 +2,15 @@
 
 # Main
 main () {
-    CONFIG=/home/ark/.config/arkos.cfg
-    . "${CONFIG}"
-    if [ "${wifi}" = true ]; then
+    if [[ "$( nmcli radio wifi )" == "enabled" ]]; then
         sudo nmcli r wifi off
-        sed -i 's/wifi=true/wifi=false/g' "${CONFIG}"
         if [[ "$( nmcli radio wifi )" != "disabled" ]]; then
             printf "\n\e[32mProblem disabling WiFi.\n"
         else
             printf "\n\e[32mWiFi has been disabled.\n"
         fi
-    elif [ "${wifi}" = false ]; then
+    elif [[ "$( nmcli radio wifi )" == "disabled" ]]; then
         sudo nmcli r wifi on
-        sed -i 's/wifi=false/wifi=true/g' "${CONFIG}"
         if [[ "$( nmcli radio wifi )" != "enabled" ]]; then
             printf "\n\e[32mProblem enabling WiFi.\n"
         else
