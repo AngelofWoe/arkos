dbfd1fe50451dcd05a4cd13b5839a6e989c0bf1e
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-23 12:35:58 -0500

    Can't be rewriting the entire file now

--- RG351P-M/Ubuntu OS Partition/opt/system/Toggle Launchimage between ASCII and JPG.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Toggle Launchimage between ASCII and JPG.sh
@@ -7,13 +7,13 @@ main () {
         if [[ "${launchimage}" == "jpg" ]]; then # Switch from JPG to ASCII
             printf "\n\e[32mChanging to launchimage.ascii\n"
             sleep 2
-            echo 'launchimage="ascii"' | tee /home/ark/.config/launchimage.cfg > /dev/null
+            sed -i 's/\(launchimage=\)"jpg"/\1"ascii"/g' /home/ark/.config/arkos.cfg
             printf "\033c" | tee -a /dev/tty1
             sudo systemctl restart emulationstation
         elif [[ "${launchimage}" == "ascii" ]]; then # Switch from ASCII to JPG
             printf "\n\e[32mChanging to launchimage.jpg\n"
             sleep 2
-            echo 'launchimage="jpg"' | tee /home/ark/.config/launchimage.cfg > /dev/null
+            sed -i 's/\(launchimage=\)"ascii"/\1"jpg"/g' /home/ark/.config/arkos.cfg
             printf "\033c" | tee -a /dev/tty1
             sudo systemctl restart emulationstation
         fi
