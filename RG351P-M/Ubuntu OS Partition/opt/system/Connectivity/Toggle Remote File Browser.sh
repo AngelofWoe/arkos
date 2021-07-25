c3f746c3a4cbe068dbd012f7b2810fa8c6b1529a
AngelofWoe <jaia.one.winged.angel@gmail.com>
2021-07-25 13:43:21 -0500

    Making the file browser a service makes it a lot easier to toggle on and off

--- RG351P-M/Ubuntu OS Partition/opt/system/Connectivity/Toggle Remote File Browser.sh
+++ RG351P-M/Ubuntu OS Partition/opt/system/Connectivity/Toggle Remote File Browser.sh
@@ -2,34 +2,33 @@
 
 # Main
 main () {
-    CONFIG=/home/ark/.config/arkos.cfg
-    . "${CONFIG}"
-    
-    if [ "${remotefilebrowser}" = false ]; then
+    clear
+    SERVICE="remote_file_browser.service"
+
+    if ! systemctl is-active --quiet "${SERVICE}"; then
         GW=$( ip route | awk '/default/ { print $3 }' )
         if [ -n "${GW}" ]; then
-            printf "\n\n\e[32mEnabling Remote File Browser.  Please wait...\n"						
-            sudo timedatectl set-ntp 1 &
-            sudo systemctl start smbd
-            sudo systemctl start nmbd
-            sudo filebrowser -a 0.0.0.0 -p 80 -d /home/ark/.config/filebrowser.db &
-            sed -i 's/remotefilebrowser=false/remotefilebrowser=true/g' "${CONFIG}"
-            printf "\n\n\n\e[32mRemote File Browser has been enabled.\n"
-            printf "Your IP is: %s\n\n" "$( ip route | awk '/src/ { print $9 }' )"
-            sleep 5
+            printf "\n\e[32mEnabling Remote File Browser.  Please wait...\n"						
+            if ! sudo systemctl start "${SERVICE}" ; then
+                printf "\n\n\e[91mUnable to start Remote File Browser.\n"
+            else
+                printf "\n\n\e[32mRemote File Browser has been enabled.\n"
+                printf "Your IP is: %s\n\n" "$( ip route | awk '/src/ { print $9 }' )"
+            fi
         else
-            printf "\n\n\n\e[91mYour network connection doesn't seem to be working. Did you make sure to configure your WiFi connection using the WiFi Connection Manager in the Options/Connectivity menu?\n"
-            sleep 5
+            printf "\n\n\e[91mYour network connection doesn't seem to be working. Did you make sure to configure your WiFi connection using the WiFi Connection Manager in the Options/Connectivity menu?\n"
         fi
-    elif [ "${remotefilebrowser}" = true ]; then
-        sudo timedatectl set-ntp 0
-        sudo systemctl stop smbd
-        sudo systemctl stop nmbd
-        sudo pkill filebrowser
-        printf "\n\n\n\e[32mRemote File Browser has been disabled.\n"
-        sed -i 's/remotefilebrowser=true/remotefilebrowser=false/g' "${CONFIG}"
-        sleep 2
+    else
+        printf "\n\e[32mDisabling Remote File Browser.  Please wait...\n"
+        if ! sudo systemctl stop "${SERVICE}" ; then
+            printf "\n\e[91mUnable to stop remote file browser\n"
+        else
+            printf "\n\n\e[32mRemote File Browser has been disabled.\n"
+        fi
     fi
+
+    sleep 5
+    clear
 }
 
 # Make sure script is running directly
